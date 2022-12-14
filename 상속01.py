327e49859d4cbb83f39ad96365216b692844fb1b
papasmf1 <papasmf1@gmail.com>
2022-12-14 11:48:16 +0900

    클래스 상속 1

--- 상속01.py
+++ 상속01.py
@@ -1,3 +1,4 @@
+#부모 클래스(공통단)
 class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
@@ -6,16 +7,19 @@ class Person:
         print("Info(Name:{0}, Phone Number: {1})".format(
             self.name, self.phoneNumber))
 
+#자식 클래스(특화된 코드)
 class Student(Person):
+    #상속받은 메서드를 재정의(덮어쓰기, override)
     def __init__(self, name, phoneNumber, subject, studentID):
         self.name = name
         self.phoneNumber = phoneNumber
         self.subject = subject
         self.studentID = studentID
 
-
+#인스턴스 생성
 p = Person("전우치", "010-222-1234")
-s = Student("이순신", "010-111-1234", "컴공", "991122")
-p.printInfo()
-s.printInfo()
+s = Student("이순신", "010-111-1234", "빅데이터", "201122")
+print( p.__dict__ )
+print( s.__dict__ )
+
 
