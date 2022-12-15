087095d7630f34459d111436b3e5caf3b536aefa
papasmf1 <papasmf1@gmail.com>
2022-12-15 14:39:40 +0900

    웹 크롤링1

---
+++ web1.py
@@ -0,0 +1,11 @@
+# web1.py
+from bs4 import BeautifulSoup
+
+#페이지 로딩
+page = open("c:\\work\\test01.html", "rt", encoding="utf-8").open() 
+#검색이 용이한 객체 
+soup = BeautifulSoup(page, "html.parser")
+#전체 페이지 보기
+print( soup.prettify() )
+
+
