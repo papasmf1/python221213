116cfbfb91be317e6b1ea71d4a92b2b431aba2fa
papasmf1 <papasmf1@gmail.com>
2022-12-15 15:25:58 +0900

    웹 크롤링 3

--- web1.py
+++ web1.py
@@ -16,7 +16,15 @@ soup = BeautifulSoup(page, "html.parser")
 #검색의 조건: <p class='outer-text'>
 #파이썬의 키워드 class제공(이름충돌): class_ 
 #print( soup.find_all("p", class_="outer-text") )
-print( soup.find_all("p", class_="outer-text") )
+#print( soup.find_all("p", class_="outer-text") )
+
+#컨텐츠만 
+for item in soup.find_all("p"):
+    title = item.text.strip() 
+    title = title.replace("\n", "")
+    print(title)
+
+
 
 
 
