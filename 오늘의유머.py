3ec4d4356a1c830c1cb1265a1a86b743eb7987c2
papasmf1 <papasmf1@gmail.com>
2022-12-15 16:39:40 +0900

    오늘의 유머 2

--- 오늘의유머.py
+++ 오늘의유머.py
@@ -26,9 +26,9 @@ for n in range(1,11):
         for item in list:
                 try:
                         title = item.find("a").text.strip() 
-                        print(title)
-                        # if (re.search('아이폰', title)):
-                        #         print(title.strip())
+                        #print(title)
+                        if (re.search('한국', title)):
+                                print(title)
                 except:
                         pass
         
